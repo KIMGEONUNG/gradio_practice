a4a81dcf104b753fd551f8481001b218cc3889fa
comar <saywooong@gmail.com>
2022-12-12 17:01:25 +0900

    update: trivial

--- color_hint_from_gray/main.py
+++ color_hint_from_gray/main.py
@@ -2,14 +2,17 @@
 
 import gradio as gr
 from skimage.color import rgb2gray
+import cv2 as cv2
 import numpy as np
 
 def gen_mask(x: np.ndarray):
-    # mask = (x[..., 0] == x[..., 1]) & (x[..., 1] == x[..., 2]) & (x[..., 2] == x[..., 0]) == False
-    # x = mask[..., None] * x
+    mask = (x[..., 0] == x[..., 1]) & (x[..., 1] == x[..., 2]) & (x[..., 2] == x[..., 0]) == False
+    x = mask[..., None] * x
+    x = cv2.resize(x, (16, 16), interpolation=cv2.INTER_NEAREST)
     return x
 
-with gr.Blocks() as demo:
+css = "img { image-rendering: pixelated; }"
+with gr.Blocks(css=css) as demo:
     with gr.Row():
         view_color = gr.Image(label="Color")
         view_gray = gr.ImagePaint(label="Gray")
@@ -18,6 +21,6 @@ with gr.Blocks() as demo:
 
     # Events
     view_color.change(lambda x: rgb2gray(x), inputs=view_color, outputs=view_gray)
-    # view_gray.change(gen_mask, inputs=view_gray, outputs=view_mask)
+    view_gray.change(gen_mask, inputs=view_gray, outputs=view_mask)
 
 demo.launch(debug=True)
